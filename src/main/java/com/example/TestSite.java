d5cde5f2800e45600f0226ee0e6a63dcbce63f6e
root <kk.suresh@tcs.com>
2020-06-19 03:06:54 +0530

    Updated POM

--- src/main/java/com/example/TestSite.java
+++ src/main/java/com/example/TestSite.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
